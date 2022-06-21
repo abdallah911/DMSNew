4c54d715166f8d636eb791c94d124558a8235c08
Abdallah Galal <abdallah_galal911@outlook.com>
2022-06-21 12:47:08 +0200

    Consumption reports

--- DMS_Authontication1/Controllers/DMSMembers/ConsumptionAdminController.cs
+++ DMS_Authontication1/Controllers/DMSMembers/ConsumptionAdminController.cs
@@ -258,6 +258,127 @@ namespace DMS_TEST.Controllers
                 throw ex;
             }
         }
+        
+        public ActionResult PrintConsumptionReportsXlx(string Regfrom, string Regto, string Serfrom, string Serto
+            , string CompanyFrom, string CompanyTo, string ProviderName, string CardFrom
+            , string CardTo, string RepotType)
+        {
+            Int64 CompanyStart, CompanyEnd,ProviderNumber1, ProviderNumber2;
+            string CardStart, CardEnd;
+            DateTime RegDateFrom, RegDateTo, SerDateFrom, SerDateTo;
+            CompanyStart = string.IsNullOrEmpty(CompanyFrom)  ? 0 : Convert.ToInt64(CompanyFrom);
+            CompanyEnd =  string.IsNullOrEmpty(CompanyTo) ? 999999999 : Convert.ToInt64(CompanyTo);
+            ProviderNumber1 = ProviderName=="null" ? 0 : Convert.ToInt64(ProviderName);
+            ProviderNumber2 = ProviderName == "null" ? 999999999 : Convert.ToInt64(ProviderName);
+            CardStart = CardFrom=="null" ? "0" : CardFrom;
+            CardEnd = CardTo=="null" ? "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" : CardTo;
+
+
+
+            RegDateFrom = string.IsNullOrEmpty(Regfrom) ? new DateTime(2017, 1, 1) : (Convert.ToDateTime(Regfrom)).Date;
+            RegDateTo = string.IsNullOrEmpty(Regto) ? DateTime.Now.Date : (Convert.ToDateTime(Regto)).Date;
+            SerDateFrom = string.IsNullOrEmpty(Serfrom) ? new DateTime(2017, 1, 1) : (Convert.ToDateTime(Serfrom)).Date;
+            SerDateTo = string.IsNullOrEmpty(Serto) ? DateTime.Now.Date : (Convert.ToDateTime(Serto)).Date;
+
+            ReportDocument rd = new ReportDocument();
+
+            switch (Convert.ToInt32(RepotType))
+            {
+                case 901:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum901.rpt"));
+                    break;
+                case 9011:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum901-1.rpt"));
+                    break;
+
+                case 902:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum902.rpt"));
+                    break;
+
+                case 903:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum903.rpt"));
+                    break;
+                case 904:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum904.rpt"));
+                    break;
+                case 905:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum905.rpt"));
+                    break;
+                case 906:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum906.rpt"));
+                    break;
+                case 907:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum907.rpt"));
+                    break;
+                case 908:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum908.rpt"));
+                    break;
+                case 909:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum909.rpt"));
+                    break;
+                case 910:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum910.rpt"));
+                    break;
+                case 911:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum911.rpt"));
+                    break;
+                case 912:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum912.rpt"));
+                    break;
+                case 913:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum913.rpt"));
+                    break;
+                case 914:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum914.rpt"));
+                    break;
+                case 915:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum915.rpt"));
+                    break;
+                case 916:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum916.rpt"));
+                    break;
+                case 917:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum917.rpt"));
+                    break;
+                case 918:
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports/ReportsConsumption"), "Consum918.rpt"));
+                    break;
+                default:
+                    return View();
+            }
+
+            rd.SetDatabaseLogon("APP", "12369");
+
+            rd.SetParameterValue("crda1", RegDateFrom);
+            rd.SetParameterValue("crda2", RegDateTo);
+            rd.SetParameterValue("srda1", SerDateFrom);
+            rd.SetParameterValue("srda2", SerDateTo);
+            rd.SetParameterValue("comp1", CompanyStart);
+            rd.SetParameterValue("comp2", CompanyEnd);
+            rd.SetParameterValue("crd1", CardStart);
+            rd.SetParameterValue("crd2", CardEnd);
+            rd.SetParameterValue("cls1", "0");
+            rd.SetParameterValue("cls2", "zzzzzzzzz");
+            rd.SetParameterValue("PRV1", ProviderNumber1);
+            rd.SetParameterValue("PRV2", ProviderNumber2);
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.ExcelRecord);
+                stream.Seek(0, SeekOrigin.Begin);
+                rd.Close();
+                rd.Dispose();
+                GC.Collect();
+                return File(stream, "application/xls", RegDateFrom.ToString("ddMMyyyy") + "Consumption.xls");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
 
         #endregion
