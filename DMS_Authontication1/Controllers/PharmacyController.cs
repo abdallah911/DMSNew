eb09859e200fbe4d42d2a9d513542a4481458090
TM <swe.tm7@gmail.com>
2023-03-09 17:53:58 +0200

    new report in pharmacy index

--- DMS_Authontication1/Controllers/PharmacyController.cs
+++ DMS_Authontication1/Controllers/PharmacyController.cs
@@ -3756,21 +3756,37 @@ namespace DMS_TEST.Controllers
                 DateTime F = Convert.ToDateTime(From);
                 DateTime T = Convert.ToDateTime(To).AddSeconds(86399);//to get all day
                 ReportDocument rd = new ReportDocument();
-                rd.Load(Path.Combine(Server.MapPath("~/Reports"), "AllClaimsReport.rpt"));
-                rd.SetDatabaseLogon("dms_report", "W?8Z?PA-C4dNvNe3");
-                rd.SetParameterValue("@from", F);
-                rd.SetParameterValue("@to", T);
-                rd.SetParameterValue("@provider", Provider);
-                rd.SetParameterValue("@Company", Company);
-                rd.SetParameterValue("@Branch", Branch);
-                rd.SetParameterValue("@ApprovalNo", ApprovalNo);
-                rd.SetParameterValue("@CardId", CardId);
-                rd.SetParameterValue("@TYPE", ddlType);
 
-                Response.Buffer = false;
-                Response.ClearContent();
-                Response.ClearHeaders();
 
+                if (ddlType == "Pharmacy_Data")
+                {
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports"), "ChronicDataReport.rpt"));
+                    rd.SetDatabaseLogon("dms_report", "W?8Z?PA-C4dNvNe3");
+                  
+                    rd.SetParameterValue("@comp", Company);
+              
+
+                    Response.Buffer = false;
+                    Response.ClearContent();
+                    Response.ClearHeaders();
+                }
+                else
+                {
+                    rd.Load(Path.Combine(Server.MapPath("~/Reports"), "AllClaimsReport.rpt"));
+                    rd.SetDatabaseLogon("dms_report", "W?8Z?PA-C4dNvNe3");
+                    rd.SetParameterValue("@from", F);
+                    rd.SetParameterValue("@to", T);
+                    rd.SetParameterValue("@provider", Provider);
+                    rd.SetParameterValue("@Company", Company);
+                    rd.SetParameterValue("@Branch", Branch);
+                    rd.SetParameterValue("@ApprovalNo", ApprovalNo);
+                    rd.SetParameterValue("@CardId", CardId);
+                    rd.SetParameterValue("@TYPE", ddlType);
+
+                    Response.Buffer = false;
+                    Response.ClearContent();
+                    Response.ClearHeaders();
+                }
                 Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.ExcelRecord);
                 stream.Seek(0, SeekOrigin.Begin);
                 rd.Close();
