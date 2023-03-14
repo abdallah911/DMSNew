b176acae03de91ac96655cd86261f740e1404cdd
Abdallah Galal <abdallah_galal911@outlook.com>
2023-03-14 13:07:05 +0200

    edit connection string

--- DMS_Authontication1/Controllers/PharmacyController.cs
+++ DMS_Authontication1/Controllers/PharmacyController.cs
@@ -4104,31 +4104,47 @@ namespace DMS_TEST.Controllers
         }
         public ActionResult CompaniesChronicDeliveryReport(string Date = "", int CompId = 0, int GroupId = 0, string CardId = "")
         {
-
-
-            DateTime DispenseDate = Convert.ToDateTime(Date);
-
+            var date = DateTime.Now;
             ReportDocument rd = new ReportDocument();
-            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "CompanyChronicDelivery.rpt"));
-            ApplicationDbContext users = new ApplicationDbContext();
-            var CurrentUser = users.Users.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
-            int ProviderId = Convert.ToInt32(CurrentUser.Provider);
+            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "ChronicDataReport.rpt"));
             rd.SetDatabaseLogon("dms_report", "W?8Z?PA-C4dNvNe3");
-            rd.SetParameterValue("@ProviderId", ProviderId);
-            rd.SetParameterValue("@ProviderName", CurrentUser.UserName);
-            rd.SetParameterValue("@CompId", CompId);
-            rd.SetParameterValue("@CardId", CardId);
-            rd.SetParameterValue("@DispenseDate", DispenseDate);
-            rd.SetParameterValue("@GroupId", GroupId);
+
+            rd.SetParameterValue("@comp", CompId);
+
             Response.Buffer = false;
             Response.ClearContent();
             Response.ClearHeaders();
-            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+
+            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.ExcelRecord);
             stream.Seek(0, SeekOrigin.Begin);
             rd.Close();
             rd.Dispose();
             GC.Collect();
-            return File(stream, "application/pdf", Date + "CompanyChronicDelivery.pdf");
+            return File(stream, "application/xls", date.ToString("ddMMyyyy") + "CompanyChronicDelivery.xls");
+
+            //DateTime DispenseDate = Convert.ToDateTime(Date);
+
+            //ReportDocument rd = new ReportDocument();
+            //rd.Load(Path.Combine(Server.MapPath("~/Reports"), "CompanyChronicDelivery.rpt"));
+            //ApplicationDbContext users = new ApplicationDbContext();
+            //var CurrentUser = users.Users.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            //int ProviderId = Convert.ToInt32(CurrentUser.Provider);
+            //rd.SetDatabaseLogon("dms_report", "W?8Z?PA-C4dNvNe3");
+            //rd.SetParameterValue("@ProviderId", ProviderId);
+            //rd.SetParameterValue("@ProviderName", CurrentUser.UserName);
+            //rd.SetParameterValue("@CompId", CompId);
+            //rd.SetParameterValue("@CardId", CardId);
+            //rd.SetParameterValue("@DispenseDate", DispenseDate);
+            //rd.SetParameterValue("@GroupId", GroupId);
+            //Response.Buffer = false;
+            //Response.ClearContent();
+            //Response.ClearHeaders();
+            //Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            //stream.Seek(0, SeekOrigin.Begin);
+            //rd.Close();
+            //rd.Dispose();
+            //GC.Collect();
+            //return File(stream, "application/pdf", Date + "CompanyChronicDelivery.pdf");
         }
         #endregion
 
