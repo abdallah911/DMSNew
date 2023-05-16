3e17ef9f122e55630e3a8056ca6568410a0c93f7
TM <swe.tm7@gmail.com>
2023-05-16 10:06:38 +0300

    update report data chronic

--- DMS_Authontication1/Controllers/PharmacyController.cs
+++ DMS_Authontication1/Controllers/PharmacyController.cs
@@ -4178,7 +4178,8 @@ namespace DMS_TEST.Controllers
                     rd.SetDatabaseLogon("dms_report", "W?8Z?PA-C4dNvNe3");
 
                     rd.SetParameterValue("@comp", Company);
-
+                    rd.SetParameterValue("@from", From);
+                    rd.SetParameterValue("@to", To);
                 }
                 else
                 {
