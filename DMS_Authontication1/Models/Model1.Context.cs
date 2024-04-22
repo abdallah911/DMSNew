438e735c855ee897abe1dbd3207a198cf23f9322
Abdallah Galal <abdallah_galal911@outlook.com>
2024-04-22 10:21:56 +0200

    edit print and  database model

--- DMS_Authontication1/Models/Model1.Context.cs
+++ DMS_Authontication1/Models/Model1.Context.cs
@@ -149,6 +149,7 @@ namespace DMS_Authontication1.Models
         public virtual DbSet<CardCode> CardCodes { get; set; }
         public virtual DbSet<ApprovalCode> ApprovalCodes { get; set; }
         public virtual DbSet<PETROTRADE_EMPLOYEES> PETROTRADE_EMPLOYEES { get; set; }
+        public virtual DbSet<PrintMedicalReport> PrintMedicalReports { get; set; }
     
         [DbFunction("DMS_TESTEntities", "fn_GetEmployessForCompany")]
         public virtual IQueryable<fn_GetEmployessForCompany_Result> fn_GetEmployessForCompany(Nullable<int> companyId, Nullable<int> maxContract, string tERMINATE_FLAG, string search)
