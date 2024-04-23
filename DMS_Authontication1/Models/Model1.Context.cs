e3a73ef9e94cf28ab9d51a7149fb554214cbf441
Abdallah Galal <abdallah_galal911@outlook.com>
2024-04-23 10:05:03 +0200

    update model for merge

--- DMS_Authontication1/Models/Model1.Context.cs
+++ DMS_Authontication1/Models/Model1.Context.cs
@@ -43,7 +43,6 @@ namespace DMS_Authontication1.Models
         public virtual DbSet<Contract_Data> Contract_Data { get; set; }
         public virtual DbSet<DailyPermission> DailyPermissions { get; set; }
         public virtual DbSet<Diagnosi> Diagnosis { get; set; }
-        public virtual DbSet<Employee_Request_Type> Employee_Request_Type { get; set; }
         public virtual DbSet<EmployeeRelation> EmployeeRelations { get; set; }
         public virtual DbSet<Gender> Genders { get; set; }
         public virtual DbSet<Governate> Governates { get; set; }
@@ -155,6 +154,7 @@ namespace DMS_Authontication1.Models
         public virtual DbSet<RenewalInsideMedicalAuthority> RenewalInsideMedicalAuthorities { get; set; }
         public virtual DbSet<RenewalOutsideMedicalAuthority> RenewalOutsideMedicalAuthorities { get; set; }
         public virtual DbSet<RenewalStepTwo> RenewalStepTwoes { get; set; }
+        public virtual DbSet<Employee_Request_Type> Employee_Request_Type { get; set; }
     
         [DbFunction("DMS_TESTEntities", "fn_GetEmployessForCompany")]
         public virtual IQueryable<fn_GetEmployessForCompany_Result> fn_GetEmployessForCompany(Nullable<int> companyId, Nullable<int> maxContract, string tERMINATE_FLAG, string search)
