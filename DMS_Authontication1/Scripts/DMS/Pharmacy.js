889b62dda7fbf17c8a02c354c8549dbdda496efb
Abdulrahman Mohamed <abdulrahmanmohamed300@gmail.com>
2024-08-12 17:05:30 +0300

    hr pages

--- DMS_Authontication1/Scripts/DMS/Pharmacy.js
+++ DMS_Authontication1/Scripts/DMS/Pharmacy.js
@@ -2568,4 +2568,28 @@ function AddNationalId() {
         }
     });
 }
-
+//========================ddldiagnosis==========================================
+$(document).ready(function () {
+    $('#expandVertical').on('click', function () {
+        $(this).addClass('d-none');
+        $('#expandHorisontal').removeClass('d-none');
+        $('.ddlDiagnoises ul#select2-ddlDiagnoises-container')
+            .css({
+                'overflow-y': 'scroll',
+                'overflow-x': 'hidden',
+                'flex-wrap': 'wrap',
+                ' height': '45px !important',
+            });
+    });
+    $('#expandHorisontal').on('click', function () {
+        $(this).addClass('d-none');
+        $('#expandVertical').removeClass('d-none');
+        $('.ddlDiagnoises ul#select2-ddlDiagnoises-container')
+            .css({
+                'overflow-y': 'hidden',
+                'overflow-x': 'scroll',
+                'flex-wrap': 'nowrap',
+                ' height': 'unset',
+            });
+    });
+});
