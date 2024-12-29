d69cc1f90ee8dac744f59476e60153c308ecd475
Abdallah Galal <abdallah_galal911@outlook.com>
2024-12-29 22:02:17 +0200

    new push from TM

--- DMS_Authontication1/Scripts/DMS/Labs/Lab.js
+++ DMS_Authontication1/Scripts/DMS/Labs/Lab.js
@@ -584,7 +584,7 @@ $(function () {
                                             Cash: $('#txtValueCash').val(),
                                             PhoneNumber: $('#PhoneNumber').val(),
                                             ClaimNumber: $('#ClaimNumber').val(),
-                                            Diagnose1: $('#Comments').val(),
+                                            //Diagnose1: $('#Comments').val(),
                                             Diagnose2: NationalId,
                                             createdby: $('#ddlUsers').val() == undefined ? null : $('#ddlUsers :selected').val(),
                                             IsFamily: $('#IsFamily').val() == '' ? null : $('#IsFamily').val(),
@@ -1166,7 +1166,7 @@ function ClearCardData() {
     $('#Co_insurance_INSURANCE_DAY_LAB').val('');
     //$('#Co_insurance_INSURANCE_MONTH_LAB').val('');
     $('#ddEmp_CEILING_PERT').val('');
-    $('#Comments').val('');
+    //$('#Comments').val('');
     $("#submit").attr("disabled", false);
 }
 function ClearLabData() {
