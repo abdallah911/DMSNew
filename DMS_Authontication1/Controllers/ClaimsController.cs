a4f88e8280ba7f90f0af7e4416e538d68775b88b
TM <swe.tm7@gmail.com>
2025-04-17 18:19:39 +0200

    claim photo

---
+++ DMS_Authontication1/Controllers/ClaimsController.cs
@@ -0,0 +1,108 @@
+﻿using DMS_Authontication1.Data_Function;
+using DMS_Authontication1.Models;
+using DMS_Authontication1.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using CrystalDecisions.CrystalReports.Engine;
+using System.IO;
+using System.Globalization;
+using System.Threading;
+using DMS_Authontication1.ViewModel.HR;
+
+
+namespace DMS_Authontication1.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        private DMS_TESTEntities db = new DMS_TESTEntities();
+        ApplicationDbContext myEntities = new ApplicationDbContext();
+        DBApproval dbData = new DBApproval();
+        DBData dbData2 = new DBData();
+
+        [HttpGet]
+        public ActionResult UploadClaimPhoto(string id = "")
+        {
+            // You can return a view or JSON, depending on what you want
+            ViewBag.stat = "Ok";
+            if (id != "")
+            {
+                Int64 ClaimId = Int64.Parse(id);
+
+                     var data = (from h in db.HospitalClaims
+                                 join sp in db.Specialities1 on h.SpecialistId equals sp.SPEC_ID
+                                 where h.IdPrimary == ClaimId
+                                 select new HospitalClaimPhotoesViewModel
+                                 {
+                                     CardId = h.CARD_ID,
+                                     ClaimNumber = h.IdPrimary,
+                                     Speciality = sp.SPEC_ANAME
+                                 }).FirstOrDefault();
+                return View(data);
+            }
+            ViewBag.stat = "NoData";
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HospitalClaimPhotoesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (db.ClaimPhotoes.Where(c => c.ClaimNumber == model.ClaimNumber).FirstOrDefault() != null)
+                    {
+                        ViewBag.error = "yes";
+                        return View(model);
+                    }
+                    
+                    var entity = new ClaimPhoto();
+                    
+                    entity.ClaimNumber = model.ClaimNumber;
+                    entity.CardId = model.CardId;
+                    entity.Speciality = model.Speciality;
+                    entity.IsDispense = false;
+                    entity.IsDespenseLab = false;
+                    entity.IsDespenseRay = false;
+                    entity.IsDespensePharm = false;
+                    entity.IsDeleted = false;
+                    entity.CreatedBy = User.Identity.Name;
+                    entity.CreatedDate = DateTime.Now;
+                    if (model.ImageFile != null)
+                    {
+                        string extension = Path.GetExtension(model.ImageFile.FileName);
+                        string fileName = "Claim " + model.ClaimNumber + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
+                        model.ImageFile.SaveAs(Server.MapPath("/Content/Claims/" + fileName /*ImageFile.FileName*/));
+                        entity.Url = "~/Content/Claims/" + fileName;
+                    }
+                    
+                    db.ClaimPhotoes.Add(entity);
+                    db.SaveChanges();
+                    //return RedirectToAction("UploadClaimPhoto");
+
+                    return RedirectToAction("UploadClaimPhoto", new { id = model.ClaimNumber });
+
+                }
+                catch (Exception ex)
+                {
+
+                    ViewBag.error = "yes";
+                    return View(model);
+                }
+
+            }
+            ViewBag.error = "yes";
+            return View(model);
+        }
+
+    }
+}
---
+++ DMS_Authontication1/ViewModel/HospitalClaimDataViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DMS_Authontication1.ViewModel
+{
+    public class HospitalClaimDataViewModel
+    {
+        public string Id { get; set; }
+        public string CardId { get; set; }
+        public string Speciality { get; set; }        
+    }
+}
