130367bd3560d32b2e3c99fc2f73621b30a0f0ba
TM <swe.tm7@gmail.com>
2025-04-22 15:05:47 +0200

    new update for screen UploadClaimPhoto

--- DMS_Authontication1/Controllers/ClaimsController.cs
+++ DMS_Authontication1/Controllers/ClaimsController.cs
@@ -103,6 +103,84 @@ namespace DMS_Authontication1.Controllers
             ViewBag.error = "yes";
             return View(model);
         }
+        [HttpPost]
+        public JsonResult getData(Int64 claimId)
+        {
+            var result = (from h in db.HospitalClaims
+                          join sp in db.Specialities1 on h.SpecialistId equals sp.SPEC_ID
+                          where h.IdPrimary == claimId
+                          select new
+                          {
+                              CardId = h.CARD_ID,
+                              ClaimNumber = h.IdPrimary,
+                              Speciality = sp.SPEC_ANAME
+                          }).FirstOrDefault();
 
+            if (result == null)
+            {
+                return Json(new { success = false, message = "لا توجد بيانات" }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { success = true, data = result }, JsonRequestBehavior.AllowGet);
+          
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadClaimPhoto(HospitalClaimPhotoesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (db.ClaimPhotoes.Where(c => c.ClaimNumber == model.ClaimNumber).FirstOrDefault() != null)
+                    {
+                        ViewBag.error = "Claim added befor for this card";
+                        return View(model);
+                    }
+
+                    var entity = new ClaimPhoto();
+                    entity.ClaimNumber = model.ClaimNumber;
+                    entity.CardId = model.CardId;
+                    entity.Speciality = model.Speciality;
+                    entity.IsDispense = false;
+                    entity.IsDespenseLab = false;
+                    entity.IsDespenseRay = false;
+                    entity.IsDespensePharm = false;
+                    entity.IsDeleted = false;
+                    entity.CreatedBy = User.Identity.Name;
+                    entity.CreatedDate = DateTime.Now;
+                    if (model.ImageFile != null)
+                    {
+                        string folderPath = Server.MapPath("~/Content/Claims/");
+                        if (!Directory.Exists(folderPath))
+                        {
+                            Directory.CreateDirectory(folderPath);
+                        }
+
+                        string extension = Path.GetExtension(model.ImageFile.FileName);
+                        string fileName = "Claim " + model.ClaimNumber + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
+                        model.ImageFile.SaveAs(Server.MapPath("/Content/Claims/" + fileName /*ImageFile.FileName*/));
+                        entity.Url = "~/Content/Claims/" + fileName;
+                    }
+                  
+                    db.ClaimPhotoes.Add(entity);
+                    db.SaveChanges();
+                    //return RedirectToAction("Index");
+                    ViewBag.error = "Saved successfully";
+                    return View();
+                   // return RedirectToAction("UploadClaimPhoto", "Claims");
+
+                }
+                catch (Exception ex)
+                {
+
+                    ViewBag.error = "There was a problem saving.";
+                    return View(model);
+                }
+
+            }
+            ViewBag.error = "Please Complete the data";         
+            return View(model);
+        }
     }
 }
